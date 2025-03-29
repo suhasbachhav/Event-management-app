66069f6a6caf162a592d12f2580e522a55f0d3ac
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-29 21:21:53 +0530

    update event

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -11,36 +11,34 @@ export default function EditEvent() {
   const navigate = useNavigate();
   const params = useParams();
 
-  const {data, isPending, isError, error} = useQuery({
+  const { data, isPending, isError, error } = useQuery({
     queryKey: ['events', params.id],
-    queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
-
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  const {
-    mutate,
-    // isPending: isPendingDeletion,
-    // isError: isErrorDeleting,
-    // error: deleteError,
-  } = useMutation({
+  const { mutate } = useMutation({
     mutationFn: updateEvent,
-    onMutate: async (data)=>{
+    onMutate: async (data) => {
       const newEvent = data.event;
-      await queryClient.cancelQueries({queryKey: ['events', params.id]});
-      const previousEvent = await queryClient.getQueryData({queryKey: ['events', params.id]});
-      queryClient.setQueryData(['events', params.id], newEvent );
 
-      return{previousEvent}
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] });
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+
+      queryClient.setQueryData(['events', params.id], newEvent);
+
+      return { previousEvent };
     },
-    onError:(error, data, context)=> {
-      queryClient.setQueryData(['events', params.id], context.previousEvent );
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
     }
   });
 
-
-
   function handleSubmit(formData) {
-    mutate({id:params.id, event:formData})
+    mutate({ id: params.id, event: formData });
+    navigate('../');
   }
 
   function handleClose() {
@@ -49,36 +47,34 @@ export default function EditEvent() {
 
   let content;
 
-  if(isPending){
+  if (isPending) {
     content = (
-      <div className='center'>
+      <div className="center">
         <LoadingIndicator />
       </div>
-    )
+    );
   }
 
   if (isError) {
     content = (
       <>
-        <div id="event-details-content" className="center">
-          <ErrorBlock
-            title="Failed to load event"
-            message={
-              error.info?.message ||
-              'Failed to fetch event data, please try again later.'
-            }
-          />
-        </div>
-        <div className='form-actions'>
-            <Link to="../" className='action'>
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            error.info?.message ||
+            'Failed to load event. Please check your inputs and try again later.'
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
             Okay
-            </Link>
+          </Link>
         </div>
       </>
     );
   }
 
-  if(data){
+  if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
@@ -88,13 +84,8 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    )
+    );
   }
 
-
-  return (
-    <Modal onClose={handleClose}>
-      {content}
-    </Modal>
-  );
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
