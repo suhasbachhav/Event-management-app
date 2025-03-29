eea377dfb4a818ac9cb8fe35acd9243594425e19
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-29 20:01:07 +0530

    mutation success

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -4,13 +4,16 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { useMutation } from '@tanstack/react-query';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
-import { createNewEvent } from '../../util/http.js';
+import { createNewEvent, queryClient } from '../../util/http.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
   const {mutate, isPending, isError, error} = useMutation({
-    mutationFn: createNewEvent
-
+    mutationFn: createNewEvent,
+    onSuccess: () =>{
+      queryClient.invalidateQueries({queryKey:['events']});
+      navigate('/events')
+    }
   })
 
   function handleSubmit(formData) {
